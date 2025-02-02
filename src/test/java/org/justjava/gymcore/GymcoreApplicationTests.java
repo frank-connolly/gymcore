c5a2791d204cc52fcb54e4a745411ee6751cd425
Frank Connolly <fconnolly1@gmail.com>
2025-02-02 21:47:20 +0100

    Initial commit

---
+++ src/test/java/org/justjava/gymcore/GymcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.justjava.gymcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
