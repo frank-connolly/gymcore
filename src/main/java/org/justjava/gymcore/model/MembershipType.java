38c2b7f34cb729c60da7dfc087979bd0767623ff
Minner22 <p_makuch@poczta.onet.pl>
2025-02-05 20:09:29 +0100

    GH-17 Add Lombok to the project and modify code to use it (#17)

--- src/main/java/org/justjava/gymcore/model/MembershipType.java
+++ src/main/java/org/justjava/gymcore/model/MembershipType.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class MembershipType {
 
         @Id
@@ -17,45 +22,9 @@ public class MembershipType {
         private String description;
         private BigDecimal price;
 
-        // Default constructor is required by JPA
-        public MembershipType() {}
-
         public MembershipType(String name, String description, BigDecimal price) {
                 this.name = name;
                 this.description = description;
-                this.price = price;
-        }
-
-        // Getters and Setters
-        public Long getId() {
-                return id;
-        }
-
-        public void setId(Long id) {
-                this.id = id;
-        }
-
-        public String getName() {
-                return name;
-        }
-
-        public void setName(String name) {
-                this.name = name;
-        }
-
-        public String getDescription() {
-                return description;
-        }
-
-        public void setDescription(String description) {
-                this.description = description;
-        }
-
-        public BigDecimal getPrice() {
-                return price;
-        }
-
-        public void setPrice(BigDecimal price) {
                 this.price = price;
         }
 }
