9ed750a98dc9e11997404c67e719b244b8402072
Minner22 <p_makuch@poczta.onet.pl>
2025-02-06 18:31:28 +0100

    GH-8 Add basic logging

--- src/main/java/org/justjava/gymcore/service/GymClassService.java
+++ src/main/java/org/justjava/gymcore/service/GymClassService.java
@@ -1,12 +1,14 @@
 package org.justjava.gymcore.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.justjava.gymcore.model.GymClass;
 import org.justjava.gymcore.repository.GymClassRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GymClassService {
@@ -14,6 +16,7 @@ public class GymClassService {
     private final GymClassRepository gymClassRepository;
 
     public GymClass createGymClass(GymClass gymClass) {
+        log.info("Creating new gym class.");
         return gymClassRepository.save(gymClass);
     }
 
@@ -26,11 +29,20 @@ public class GymClassService {
     }
 
     public GymClass updateGymClass(Long id, GymClass gymClassDetails) {
+        checkIfGymClassExists(id);
         gymClassDetails.setId(id);
         return gymClassRepository.save(gymClassDetails);
     }
 
     public void deleteGymClass(Long id) {
+        checkIfGymClassExists(id);
         gymClassRepository.deleteById(id);
+    }
+
+    private void checkIfGymClassExists(Long id) {
+        if (!gymClassRepository.existsById(id)) {
+            log.warn("Cannot proceed - Gym class ID {} not found", id);
+            throw new IllegalArgumentException("Gym class not found");
+        }
     }
 }
