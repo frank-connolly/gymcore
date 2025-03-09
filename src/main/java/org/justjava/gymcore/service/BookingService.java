784148fbcd1d25bb1aa9b8ed7f31e86809598754
Netsai <hellonetsai@gmail.com>
2025-03-09 21:32:17 +0200

    Update src/main/java/org/justjava/gymcore/service/BookingService.java
    Co-authored-by: özkan pakdil <ozkan.pakdil@gmail.com>

--- src/main/java/org/justjava/gymcore/service/BookingService.java
+++ src/main/java/org/justjava/gymcore/service/BookingService.java
@@ -58,7 +58,7 @@ public class BookingService {
 
         Waitlist waitlistEntry = new Waitlist(user, gymClass);
         waitlistRepository.save(waitlistEntry);
-        log.info("User {} added to waitlist for class {}", user.getId(), gymClass.getId());
+        log.info("User {} added to waitlist for GymClass {}", user.getId(), gymClass.getId());
         return new ResponseEntity<>(new ResponseDetail("GymClass is full. You are now on the waitlist."), HttpStatus.OK);
     }
 
